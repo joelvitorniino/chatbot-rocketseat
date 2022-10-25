b4d500f35d1285d588184d52f4f4dff30160e97c
joelvitorniino <joelprofessor13@gmail.com>
2022-10-25 13:16:18 -0300

    model chat

---
+++ src/model/Chat.ts
@@ -0,0 +1,12 @@
+import { db } from "../db/db";
+import { DataTypes } from 'sequelize';
+
+export const Chat = db.define('messages', {
+    message_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    message_author: DataTypes.STRING,
+    message: DataTypes.STRING
+});
