d72a6fa51a9f861924273343d72ff377ec6d7f8a
joelvitorniino <joelprofessor13@gmail.com>
2022-11-02 08:00:26 -0300

    create route of sendEmail

--- src/routes.ts
+++ src/routes.ts
@@ -11,6 +11,7 @@ const chatController = new ChatController();
 
 router.get('/api/v1/register', registerController.index);
 router.post('/api/v1/register', registerController.store);
+router.post('/api/v1/email', registerController.sendEmail);
 router.post('/api/v1/auth', authController.authenticate);
 router.get('/api/v1/token', authMiddleware, chatController.verifyToken);
 router.delete('/api/v1/register/:id_chat', registerController.deleteById);
