2c9bc8cf210e8af6f9242093c093fb7f423b1c40
joelvitorniino <joelprofessor13@gmail.com>
2022-11-18 10:24:41 -0300

    add user of oAuth2 google in register db

--- src/app/services/google/GoogleService.ts
+++ src/app/services/google/GoogleService.ts
@@ -9,11 +9,12 @@ const repository = new RegisterRepository();
 export class GoogleService {
     async callback(request: Request, accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<VerifyFunctionWithRequest | void> {
         const { name, email } = profile;
+        const { givenName, familyName } = name;
 
         await repository.create({
-            name_chat: name,
+            name_chat: `${givenName} ${familyName}`,
             email_chat: email,
-            password_chat: null
+            password_chat: ''
         });
 
         return done(null, profile);
