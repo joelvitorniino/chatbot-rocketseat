7b13492b0fbb3b270c67ecc6eb7f40cbcf61d8a6
joelvitorniino <joelprofessor13@gmail.com>
2022-11-18 11:50:52 -0300

    add verification of token of google is valid

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,19 +3,33 @@ import { AuthController } from "./controllers/AuthController";
 import { ChatController } from "./controllers/ChatController";
 import { RegisterController } from "./controllers/RegisterController";
 import authMiddleware from "./middleware/authMiddleware";
+import { GoogleService } from "./services/google/GoogleService";
 
 const router = Router();
 const registerController = new RegisterController();
 const authController = new AuthController();
 const chatController = new ChatController();
+const passport = new GoogleService().createPassport();
 
 router.get('/api/v1/register', registerController.index);
 router.post('/api/v1/register', registerController.store);
+
 router.post('/api/v1/email', registerController.sendEmail);
 router.post('/api/v1/auth', authController.authenticate);
+
+router.post('/api/v1/auth_google', authController.authenticateGoogle);
+
 router.get('/api/v1/token', authMiddleware, chatController.verifyToken);
+
 router.post('/api/v1/forgot_password', authController.forgotPassword);
+
 router.post('/api/v1/reset_password', authController.resetPassword);
 router.delete('/api/v1/register/:id_chat', registerController.deleteById);
+
+router.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }))
+router.get('/google/callback', passport.authenticate('google', {
+    successRedirect: '/auth_google',
+    failureRedirect: '/auth/failure',
+}));
 
 export default router;
--- src/app/server.ts
+++ src/app/server.ts
@@ -5,10 +5,16 @@ import ejs from "ejs";
 import { ChatController } from "./controllers/ChatController";
 import routes from "./routes";
 import authMiddleware from "./middleware/authMiddleware";
+import { GoogleService } from "./services/google/GoogleService";
+import session from "express-session";
+import { config } from "dotenv";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const passport = new GoogleService().createPassport();
+
+config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +23,7 @@ const makeDirName = (path: string) => `${__dirname}/${path}`;
 
 const views = [
   makeDirName(`../public/home`),
+  makeDirName(`../public/auth_google`),
   makeDirName('../public/forgot'),
   makeDirName('../public/reset_password'),
   makeDirName(`../public/register`),
@@ -30,6 +37,10 @@ app.set("view engine", "html");
 
 app.get("/", (request: Request, response: Response) => {
     response.render("home.html");
+});
+
+app.get('/auth_google', (request: Request, response: Response) => {
+    response.render("auth_google.html");
 });
 
 app.get("/forgot", (request: Request, response: Response) => {
@@ -48,7 +59,16 @@ app.get("/chat", (request: Request, response: Response) => {
     response.render("chat.html");
 });
 
+app.use(session({ secret: process.env.TOKEN }))
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(routes);
+
+const googleService = new GoogleService();
+
+googleService.serializeUser();
+googleService.deserializeUser();
 
 io.on("connection", async (socket) => {
   console.log(`Socket conectado ${socket.id}`);
